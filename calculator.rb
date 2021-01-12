80870bbed3c972095539bdb6c7da03bd630a5322
Brian Cheung <briantcheung@gmail.com>
2021-01-12 02:13:23 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+
+first_number = 42
+second_number = 1027
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
